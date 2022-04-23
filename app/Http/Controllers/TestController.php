3356e06b47f9838d96d830682057d085e481c677
tatevikg1 <tatevikg1@gmail.com>
2022-04-23 22:38:34 +0400

    init luezoid

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Luezoid\Laravelcore\Http\Controllers\ApiController;
+
+
+class TestController extends ApiController
+{
+    //
+
+    public function test()
+    {
+        return true;
+    }
+}
---
+++ app/Http/Requests/TestRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Luezoid\Laravelcore\Requests\BaseRequest;
+
+class TestRequest extends BaseRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
