8ff835be11734952d8d64dac3ea98b71c71ebe13
tatevikg1 <tatevikg1@gmail.com>
2022-05-28 21:02:16 +0400

    other thing

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TestRequest;
 use Luezoid\Laravelcore\Http\Controllers\ApiController;
 
-
 class TestController extends ApiController
 {
+    
     public function test(TestRequest $request)
     {
-        return $request->validated();
+        return "somethig else";
     }
-}
+}   
